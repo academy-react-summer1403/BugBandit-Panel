bc9d30d41bf6b81102a4a845faccff23ed0ec230
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-12-07 11:54:14 +0330

    dashboard

--- panel/src/components/Courses/CourseList/index.jsx
+++ panel/src/components/Courses/CourseList/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DataTableAdvSearch from '../../../views/tables/data-tables/advance/TableAdvSearch';
 
 const CourseList = () => {
     return (
         <div>
-            
+            <DataTableAdvSearch/>
         </div>
     );
 }
--- panel/src/components/Dashboard/DashboardHolder/index.jsx
+++ panel/src/components/Dashboard/DashboardHolder/index.jsx
@@ -36,41 +36,16 @@ const DashboardHolder = () => {
       </Row>
       <Row className="match-height">
         <Col lg="4" md="12">
-          <Row className="match-height">
-            <Col lg="6" md="3" xs="6">
-              <OrdersBarChart warning={colors.warning.main} />
-            </Col>
-            <Col lg="6" md="3" xs="6">
-              <ProfitLineChart info={colors.info.main} />
-            </Col>
-            <Col lg="12" md="6" xs="12">
-              <Earnings success={colors.success.main} />
-            </Col>
-          </Row>
+          <Col lg="12" md="6" xs="12">
+            <Earnings success={colors.success.main} />
+          </Col>
         </Col>
-        <Col lg="8" md="12">
-          <RevenueReport
-            primary={colors.primary.main}
-            warning={colors.warning.main}
-          />
-        </Col>
-      </Row>
-      <Row className="match-height">
-        {/* <Col lg="8" xs="12">
-          <CompanyTable />
-        </Col>
-        <Col lg="4" md="6" xs="12">
-          <CardMeetup />
-        </Col> */}
         <Col lg="4" md="6" xs="12">
           <CardBrowserStates colors={colors} trackBgColor={trackBgColor} />
         </Col>
         <Col lg="4" md="6" xs="12" className="border">
           <GoalOverview success={colors.success.main} />
         </Col>
-        {/* <Col lg="4" md="6" xs="12">
-          <CardTransactions />
-        </Col> */}
       </Row>
     </div>
   );
